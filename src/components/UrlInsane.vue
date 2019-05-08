4e82fcd99f5c5868c0a65f7576b6446ba9db5ddb
Jubair Saidi <jubair@schireson.com>
2019-05-08 08:01:39 -0400

    Adds gitlab CI config
    Cleans up lint errors

--- src/components/UrlInsane.vue
+++ src/components/UrlInsane.vue
@@ -2,7 +2,11 @@
   .tile.is-ancestor
     transition(name="fade" mode="out-in")
       .tile.is-parent(v-if="!$store.state.urlInsane.loading")
-        FieldBuilder.tile.is-child(v-for="(option, key) in $store.state.urlInsane.options" :name="key" :option="option")
+        FieldBuilder.tile.is-child(
+          v-for="(option, key) in $store.state.urlInsane.options"
+          :name="key"
+          :option="option"
+        )
       .tile.is-vertical.is-16(v-else) Loading
 </template>
 
@@ -10,9 +14,9 @@
 import { Component, Vue } from 'vue-property-decorator';
 import FieldBuilder from './fields/FieldBuilder.vue';
 
-const components = { FieldBuilder }
+const components = { FieldBuilder };
 @Component({
-  components
+  components,
 })
 export default class UrlInsane extends Vue {
   private mounted() {
